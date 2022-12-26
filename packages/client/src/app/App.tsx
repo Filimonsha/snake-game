aa4af513ba690e02e8c20dd5a8aa884592916193
Anastasia <ashmeleva94@gmail.com>
2022-12-26 20:08:30 +0000

    leader-board markup

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,11 +1,12 @@
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { FORUM_ROUTE, MAIN_ROUTE, SIGN_IN_ROUTE, SIGN_OUT_ROUTE } from '../utils/const/route'
+import { FORUM_ROUTE, MAIN_ROUTE, SIGN_IN_ROUTE, SIGN_OUT_ROUTE, LEADERBOARD_ROUTE } from '../utils/const/route'
 import { Main } from '../pages/main'
 import { SignIn } from '../pages/sign-in'
 import { SignUp } from '../pages/sign-up'
 import { Forum } from '../pages/forum'
+import { LeaderBoard } from '../pages/main/modules/leader-board'
 
 function RequireAuth({ children }: { children: JSX.Element }) {
   // Когда будем подключать аунтификацию - будет использоваться обертка, для проверки авторизован ли пользователь,
@@ -35,6 +36,7 @@ function App() {
           <Route path={SIGN_OUT_ROUTE} element={<SignUp />} />
           <Route path={MAIN_ROUTE} element={<RequireAuth><Main /></RequireAuth>} />
           <Route path={FORUM_ROUTE} element={<Forum />} />
+          <Route path={LEADERBOARD_ROUTE} element={<LeaderBoard />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- packages/client/src/pages/main/modules/leader-board/LeaderBoard.tsx
+++ packages/client/src/pages/main/modules/leader-board/LeaderBoard.tsx
@@ -1,10 +1,72 @@
 import React from 'react'
+import './leaderBoard.scss'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import DataTable, { TableColumn } from 'react-data-table-component'
+import dummyData from './dummy.json'
 
-const LeaderBoard = () => {
+interface DataRow {
+  rank: number;
+  avatar: string;
+  user: string;
+  score: number;
+}
+
+const LeaderBoard: React.FC = () => {
+  const defaultAvatar = 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
+  const columns: TableColumn<DataRow>[] = [
+    {
+      name: 'Rank',
+      selector: row => row.rank,
+      sortable: true,
+    },
+    {
+      name: 'Avatar',
+      cell: (row) => <img className='table-img' src={row.avatar || defaultAvatar} />,
+      sortable: false,
+    },
+    {
+      name: 'User',
+      selector: row => row.user,
+      sortable: true,
+    },
+    {
+      name: 'Score',
+      selector: row => row.score,
+      sortable: true,
+    },
+  ];
+
+  const data = dummyData;
+  const topPlayersData = dummyData.sort((a, b) => b.score - a.score).slice(0, 3);
+
   return (
-    <div>
-
-    </div>
+    <Container className='p-5 d-flex flex-column'>
+      <div className='wrapper-top'>
+        <h1 className='mb-3'>Top Players</h1>
+        <div className='d-flex justify-content-evenly mb-3'>
+          {topPlayersData && topPlayersData.map(data => 
+            <Card key={data.id}>
+              <Card.Img src={data.avatar} />
+              <Card.Body>
+                <Card.Title>{data.user}</Card.Title>
+                <Card.Text>{data.score}</Card.Text>
+              </Card.Body>
+            </Card>
+          )} 
+        </div>
+      </div>
+      <div className='wrapper-bottom'>
+        <h2 className='mb-3'>All Users</h2>
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination={true}
+          paginationPerPage={5}
+          paginationRowsPerPageOptions={[5,10]}
+        />
+      </div>
+    </Container>
   )
 }
 
