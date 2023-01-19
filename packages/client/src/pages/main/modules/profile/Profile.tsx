dd1a74ccb9a6f27f70c9598bfd61625b00ed1915
Anastasia <ashmeleva94@gmail.com>
2023-01-19 18:16:33 +0000

    theme for auth, profile, leaderboard pages

--- packages/client/src/pages/main/modules/profile/Profile.tsx
+++ packages/client/src/pages/main/modules/profile/Profile.tsx
@@ -56,82 +56,86 @@ const Profile: React.FC = () => {
   }
 
   return (
-    <Container className={styles.container}>
-      <Formik
-        enableReinitialize={true}
-        initialValues={user}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ errors, values }) => (
-          <Form className={styles.form}> 
-            <div className={styles.formAvatar} onClick={handleAvatarChange}>
-              <img src={values.avatar || user.avatar} alt='avatar' className={styles.avatarImg}></img>
-            </div>
-            <div className={styles.formBody}>
-              <FormField 
-                id='first_name'
-                title='Name'
-                type='text'
-                name='first_name'
-                value={values.first_name}
-                error={errors.first_name}
-              />
-              <FormField 
-                id='second_name'
-                title='Second name'
-                type='text'
-                name='second_name'
-                value={values.second_name}
-                error={errors.second_name}
-              />
-              <FormField 
-                id='login'
-                title='Nickname'
-                type='text'
-                name='login'
-                value={values.login}
-                error={errors.login}
-              />
-              <FormField 
-                id='email'
-                title='Email'
-                type='email'
-                name='email'
-                value={values.email}
-                error={errors.email}
-              />
-              <FormField 
-                id='phone'
-                title='Phone'
-                type='phone'
-                name='phone'
-                value={values.phone}
-                error={errors.phone}
-              />
-              <FormField 
-                id='password'
-                title='Password'
-                type='password'
-                name='password'
-                value={values.password}
-                error={errors.password}
-              />
-            </div>
-            <div className={styles.formFooter}>
-              <Button className={styles.button} type='submit'>
-                Save
-              </Button>
-              <Button className={styles.button} variant='secondary' onClick={handleCancel}>
-                Cancel
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+    <div className={styles.profile}>
+      <div className={styles.profileCircle}>
+        <Container className={styles.container}>
+          <Formik
+            enableReinitialize={true}
+            initialValues={user}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, values }) => (
+              <Form className={styles.form}> 
+                <div className={styles.formAvatar} onClick={handleAvatarChange}>
+                  <img src={values.avatar || user.avatar} alt='avatar' className={styles.avatarImg}></img>
+                </div>
+                <div className={styles.formBody}>
+                  <FormField 
+                    id='first_name'
+                    title='Name'
+                    type='text'
+                    name='first_name'
+                    value={values.first_name}
+                    error={errors.first_name}
+                  />
+                  <FormField 
+                    id='second_name'
+                    title='Second name'
+                    type='text'
+                    name='second_name'
+                    value={values.second_name}
+                    error={errors.second_name}
+                  />
+                  <FormField 
+                    id='login'
+                    title='Nickname'
+                    type='text'
+                    name='login'
+                    value={values.login}
+                    error={errors.login}
+                  />
+                  <FormField 
+                    id='email'
+                    title='Email'
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    error={errors.email}
+                  />
+                  <FormField 
+                    id='phone'
+                    title='Phone'
+                    type='phone'
+                    name='phone'
+                    value={values.phone}
+                    error={errors.phone}
+                  />
+                  <FormField 
+                    id='password'
+                    title='Password'
+                    type='password'
+                    name='password'
+                    value={values.password}
+                    error={errors.password}
+                  />
+                </div>
+                <div className={styles.formFooter}>
+                  <Button className={styles.buttonSave} type='submit'>
+                    Save
+                  </Button>
+                  <Button className={styles.buttonCancel} variant='secondary' onClick={handleCancel}>
+                    Cancel
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
 
-      <AvatarModal isShown={isAvatarShown} handleClose={handleAvatarClose} handleSave={handleAvatarSave} handleDelete={handleAvatarDelete} />
-    </Container>
+          <AvatarModal isShown={isAvatarShown} handleClose={handleAvatarClose} handleSave={handleAvatarSave} handleDelete={handleAvatarDelete} />
+        </Container>
+      </div>
+    </div>
   )
 }
 
