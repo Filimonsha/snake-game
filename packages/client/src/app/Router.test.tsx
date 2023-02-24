63e21f5594ae8c3a3dc4e5b391b7e952291f71d6
nastydolgova <nasty.dolgova@gmail.com>
2023-02-24 18:09:07 +0300

    delete forum test add router test

--- packages/client/src/app/Router.test.tsx
+++ packages/client/src/app/Router.test.tsx
@@ -1,6 +1,37 @@
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+import '@testing-library/jest-dom'
+import {App, LocationDisplay} from './AppTest'
+import {BrowserRouter, MemoryRouter} from 'react-router-dom'
 
-//TODO тест роутера
+test('full app rendering/navigating', async () => {
+    render(<App />, {wrapper: BrowserRouter})
+    // verify page content for default route
+    expect(screen.getByText(/you are home/i)).toBeInTheDocument()
+})
 
-// https://testing-library.com/docs/example-react-router/
+test('landing on a bad page', () => {
+    const badRoute = '/some/bad/route'
+    // use <MemoryRouter> when you want to manually control the history
+    render(
+        <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+        </MemoryRouter>,
+    )
+    // verify navigation to "no match" route
+    expect(screen.getByText(/no match/i)).toBeInTheDocument()
+})
 
-// https://habr.com/ru/company/elbrusbootcamp/blog/651033/
+test('rendering a component that uses useLocation', () => {
+    const route = '/some-route'
+
+    // use <MemoryRouter> when you want to manually control the history
+    render(
+        <MemoryRouter initialEntries={[route]}>
+        <LocationDisplay />
+        </MemoryRouter>,
+    )
+
+    // verify location display is rendered
+    expect(screen.getByTestId('location-display')).toHaveTextContent(route)
+})
