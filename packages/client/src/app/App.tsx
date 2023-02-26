29decdf68fc21907350635d4344a33a3b46328d1
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-02-26 13:12:26 +0300

    fix/paths

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -10,15 +10,15 @@ import { LeaderBoard } from '../pages/main/modules/leaderBoard'
 import { Profile } from '../pages/main/modules/profile'
 import { Game } from '../pages/main/modules/game'
 import { ErrorPage } from '../pages/error'
-import { 
-  FORUM_ROUTE, 
-  MAIN_ROUTE, 
-  SIGN_IN_ROUTE, 
-  SIGN_UP_ROUTE, 
-  LEADERBOARD_ROUTE, 
-  PROFILE_ROUTE, 
+import {
+  FORUM_ROUTE,
+  MAIN_ROUTE,
+  SIGN_IN_ROUTE,
+  SIGN_UP_ROUTE,
+  LEADERBOARD_ROUTE,
+  PROFILE_ROUTE,
   GAME_ROUTE,
-  ERROR_ROUTE } from '../utils/const/route'
+  ERROR_ROUTE } from '../const/route'
 
 function RequireAuth({ children }: { children: JSX.Element }) {
   // Когда будем подключать аунтификацию - будет использоваться обертка, для проверки авторизован ли пользователь,
--- packages/client/src/pages/error/error.tsx
+++ packages/client/src/pages/error/error.tsx
@@ -1,6 +1,6 @@
 import styles from './error.module.scss';
 import { Link } from 'react-router-dom';
-import { MAIN_ROUTE } from '../../utils/const/route';
+import { MAIN_ROUTE } from '../../const/route';
 
 interface IErrorPage {
   title: string,
@@ -16,7 +16,7 @@ const ErrorPage: React.FC<IErrorPage> = ({ title, code }) => (
       <p className={styles.errorPageCode}>
         {code}
       </p>
-      <Link 
+      <Link
         to={MAIN_ROUTE}
         className={styles.errorPageLink}
       >
