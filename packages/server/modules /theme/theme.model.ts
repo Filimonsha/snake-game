3bf44f3f916443c6945d80fea04cb07734aa69ac
ff1892 <hitrulein@gmail.com>
2023-02-28 14:37:49 +0300

    fix/change themes model

--- packages/server/modules /theme/theme.model.ts
+++ packages/server/modules /theme/theme.model.ts
@@ -1,12 +1,15 @@
-import { AllowNull, Column, Model, Table } from 'sequelize-typescript'
+import { AllowNull, Column, Model, PrimaryKey, Table } from 'sequelize-typescript'
 
 export type TThemeSite = {
   idUser?: string,
   theme: string,
 }
 
-@Table
+@Table({ 
+  timestamps: false 
+})
 export class ThemeSite extends Model<TThemeSite> {
+  @PrimaryKey
   @AllowNull(false)
   @Column
   idUser!: string
