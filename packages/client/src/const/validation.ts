ed86879d01dfec49e00969f1c8ae19cb641b739d
Anastasia <ashmeleva94@gmail.com>
2023-03-22 13:01:23 +0000

    bug fix

--- packages/client/src/const/validation.ts
+++ packages/client/src/const/validation.ts
@@ -1,7 +1,7 @@
 export const Pattern = {
   Login: /^[a-z]{1}[a-z\d_-]{2,19}$/i,
   Password: /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,40}$/,
-  Name: /^[A-Z][A-Za-z]{0,20}[-\s]?[A-Za-z]{1,20}$/,
+  Name: /[\p{Lu}][\p{Letter}\p{Mark}\s]{1,20}/gu,
   Email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/,
   Phone: /^[+\d][\d]{10,15}$/,
 }
--- packages/client/src/pages/main/modules/leaderBoard/components/LeaderBoardAvatar/LeaderBoardAvatar.tsx
+++ packages/client/src/pages/main/modules/leaderBoard/components/LeaderBoardAvatar/LeaderBoardAvatar.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from './leaderBoardAvatar.module.scss'
-import { AVATAR_PLACEHOLDER } from '../../../../../../const/host'
 
 
 const LeaderBoardAvatar: React.FC<{ avatar: string }> = ({ avatar }): JSX.Element => {
   return(
     <img className={styles.table_img}
-      src={avatar || AVATAR_PLACEHOLDER}
+      src={avatar}
       crossOrigin='anonymous'
     />
   )
